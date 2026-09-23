c2b9ef47d63bf9a993e6b635b9ef9651371f7085
Elizaveta Maliugina <emaliugina@student.cscc.edu>
2026-09-23 00:50:47 -0400

    "Restructured the first path choice input to avoid invalid input, finished the are_you_sure function, created end_game function and fail_scenarios (both need to be finished) based on the fail variable, and began the fork to path 1."

--- Text_Adventure_Game.py
+++ Text_Adventure_Game.py
@@ -1,7 +1,7 @@
 """Name: Text_Adventure_Game
    Author: Elizaveta Maliugina
    Purpose: Project 1 for CSCI 1511
-   Date: 9/22/2026 """
+   Date: 9/27/2026 """
 
 name = str(input("Welcome! Enter your name:  "))
 
@@ -9,20 +9,55 @@ print()
 print("You arrive at the edge of a forest.")
 print()
 
-try:
-    path_chosen = int(input("There are three paths to choose from, one leads staight into the woods, another towards a hut barely visible from the edge of the forest, and one leading around the woodland, dissapearing around the trees and hills. \n\nChoose a path: 1, 2, or 3.  "))
-except ValueError:
-    print()
-    path_choosen = input("Try again, enter 1, 2, or 3 for your choosen route.  ")
+import random
+
+fail = 0
 
 def are_you_sure():
     """ Active after the user enters an incorrect form of input, asking for confirmation. If the incorrect input is validated, the function trips a randomized event and the game ends. If not, the user is asked for another input."""
-    #Add later
-    print("'are_you_sure' not added yet")
+    print()
+    confirmation = input("Are you sure about your answer? Yes or No  ")
+    print()
+    if confirmation != "yes" or confirmation != "Yes" or confirmation != "y" or confirmation != "Y":
+        print(random.choice(fail_scenarios))
+        fail = 1
+    else:
+        path_chosen = input("Please enter your desired answer:  ")
     pass
 
-print()
+fail_scenarios = ["Add later 1", "Add later 2", "Add later 3"]
 
-if path_choosen != 1 or path_choosen != 2 or path_choosen != 3:
-    are_you_sure()
+limiter = 0
 
+while True:
+    try:
+        path_chosen = int(input("There are three paths to choose from, one leads staight into the woods, another towards a hut barely visible from the edge of the forest, and one leading around the woodland, dissapearing around the trees and hills. \n\nChoose a path: 1, 2, or 3.  "))
+        if path_chosen not in [1,2,3]:
+            are_you_sure()
+            limiter += 1
+        if limiter >= 3:
+            #Add later
+            print("Limit Reached")
+            break
+        if fail == 1:
+            break
+    except ValueError:
+        print()
+        print("Invalid input. Please enter a number (1, 2, or 3).")
+        print()
+    if type(path_chosen) == int:
+        print()
+        print("Loop Complete - Remove/Replace later")
+        print()
+        break
+    else:
+        continue
+
+def end_game():
+    """Ends the game when the fail variable is 1"""
+    #Add later
+    pass
+
+#Path 1 Route       
+if path_chosen == 1:
+    path_1 = input("Placeholder")
